10e30281a6e32970e1972e3878390e9af8e7c7c6
alberbenan <57706512+alberbenan@users.noreply.github.com>
2024-10-13 19:37:39 +0200

    Update checkdata.php

--- checkdata.php
+++ checkdata.php
@@ -1,2 +1,21 @@
 <?php
 //This script is to show the validated data from contact.php
+<?php
+session_start();
+require 'functions.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $errors = validate_form_data($_POST);
+
+    if (empty($errors)) {
+        $_SESSION['contact_data'] = $_POST;
+        header("Location: checkdata.php");
+        echo "<p style='color:red;'>VALIDATE OK</p>";        
+        exit;
+    } else {
+        foreach ($errors as $error) {
+            echo "<p style='color:red;'>$error</p>";
+        }
+    }
+}
+?>
