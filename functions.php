a01af410a779b8888c605179e6ced1c76d47a4db
alberbenan <57706512+alberbenan@users.noreply.github.com>
2024-10-13 19:37:01 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,2 +1,34 @@
 <?php
 //In this script, place the functions
+session_start();
+
+function validateFormData($data) {
+    $errors = [];
+
+    if (empty(trim($data['first_name']))) {
+        $errors[] = "First name is required.";
+    }
+
+    if (empty(trim($data['surname']))) {
+        $errors[] = "Surname is required.";
+    }
+
+    if (empty($data['title'])) {
+        $data['title'] = "Mr.";
+    }
+
+    if (empty(trim($data['phone']))) {
+        $errors[] = "Phone number is required.";
+    } elseif (!preg_match("/^[0-9]{10}$/", $data['phone'])) {
+        $errors[] = "Phone number must be 10 digits.";
+    }
+
+    if (empty(trim($data['email']))) {
+        $errors[] = "Email is required.";
+    } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email format.";
+    }
+
+    return $errors;
+}
+?>
